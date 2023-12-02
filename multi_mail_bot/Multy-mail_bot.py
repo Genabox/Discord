686e254a86e3e936f3f2cfb22e7e2ff4e5ccf11d
Genabox <125927009+Genabox@users.noreply.github.com>
2023-12-02 18:01:44 +0200

    Update Multy-mail_bot.py

--- multi_mail_bot/Multy-mail_bot.py
+++ multi_mail_bot/Multy-mail_bot.py
@@ -1,1 +1,109 @@
-test
+                                            
+#                                                        ┓ ┏   ┏┓   ┳┓   ┏┳┓   ┏┓   ┳┓
+#                                                        ┃┃┃   ┣┫   ┃┃    ┃    ┣    ┃┃
+#                                                        ┗┻┛   ┛┗   ┛┗    ┻    ┗┛   ┻┛
+#               
+#                                         A         ****   S T A T E  S H E R I F F   ****         A 
+
+  ########################################################    ########################################################    ########################################### 
+    #                                     ✦        *        *        *         #          ✦        *        *        *        *  #          ✦                         $#
+    # ooo        ooooo             oooo      .                                                    o8o  oooo        .o8                     .                        #$
+    #  `88.       .888'             `888    .o8                                                    `"'  `888       "888                   .o8                   czd
+    #   888b     d'888  oooo  oooo   888  .o888oo oooo    ooo         ooo. .oo.  .oo.    .oooo.   oooo   888        888oooo.   .ooooo.  .o888oo 
+    #   8 Y88. .P  888  `888  `888   888    888    `88.  .8'          `888P"Y88bP"Y88b  `P  )88b  `888   888        d88' `88b d88' `88b   888                   nn
+    #   8  `888'   888   888   888   888    888     `88..8'   8888888  888   888   888   .oP"888   888   888        888   888 888   888   888               sdfds
+    #   8    Y     888   888   888   888    888 .    `888'             888   888   888  d8(  888   888   888        888   888 888   888   888 . 
+    #  o8o        o888o  `V88V"V8P' o888o   "888"     .8'             o888o o888o o888o `Y888""8o o888o o888o       `Y8bod8P' `Y8bod8P'   "888"         dfsdf
+    #                                            .o..P'                                                                                        
+    #                                           `Y8P'                                       ✦        *        *        ✦        *        *        *   d3%#
+    #          *        ✦       *        *        *                                                                                                                                      
+    ########################################################  ########################################################  ########################### 
+
+    ###################################################
+    #   #########  #########  #########  #########    #    ?***********
+    #                                                 # ?***********
+""""#  .......................                 """    #           ?***********"""
+""""#  .......................                 """    #       ?***********
+""""#  .......................                 """    # * **********
+    ##                                                #        
+    ##                                                #                                                           
+    ##                                                #?***********?***********
+    ##                                                #     ?***********
+    #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
+    ###################################################
+
+        #______________________________________
+        
+                # F U T U R E S:
+        #______________________________________
+
+            # - Make a new letter:
+
+        #/newletter (type /newletter to send a new letter) Example: #(line1) '/newletter'
+        # Type the recipient's email on line number two Example: #(line2) 'email@email'
+        # Type the subject on the third line Example: #(line3) 'Subject'
+        # Type the message text on line number 4 and other lines of the message Example: #(line4) 'Mail body text'
+
+        # So you need to set up 4 lines to send your new mail:
+
+        #/newletter
+        #email@email
+        #my letter subject
+        #my letter text
+
+        # That is all.
+        # ``````````````````````
+
+            # - Check mail:
+
+        #/mail - Check mail...
+        # ``````````````````````
+        
+            # - Sending a reply:
+
+        # Send a reply to a message on mail in a Discord channel - the email will be sent to the sender's mailbox @
+        # ``````````````````````
+
+            # - Software multi-mail bot description:
+
+        # Add multiple accounts as you need, one per 1 text channel. Each text channel works like a mailbox for one account. Use another text channel for a second mail account, and set
+        # channel IDs to configure it. Don't forget emojis to design your message folder style (Discord channel name setup, just copy and paste your favorite emoji into the text field)
+
+        # Set up folders to receive letters from them
+
+        # All new reader letters are marked as READ on your mail server and are not uploaded again to your channel
+
+        # Send links or files as attachments
+
+        # Set Interval in seconds to fetch your mailbox
+
+        # You can set up music files from the archive to set notifications on new letters
+
+        # /helpme - get help on chat
+
+        # If you see an authentication problem, then you need to create access to other software and make a technical password on your mail provider in your account to allow access to this bot.
+        # ``````````````````````
+
+            # - Bot setup:
+
+        # Add a new program to the Discord developer's account, authorize it on your Discord server
+
+        # Set your new token to the script
+
+        # Set up your mailboxes, folders to receive from them
+
+        # Add a mail category to your server
+
+        # Set up channels on your server and set IDs to the script; you can also set up your contacts' personal channels in the mail category
+
+        # Run the Python script; as a result, you will see "Bot online," and you will see new mail on your channels
+
+        # ``````````````````````
+
+                                                   # if you want, say thanks ~ btc: 14CZG7Tp9vyHLxJoCi1FYKzgyjeG3BuPMe
+
+                                                                                          # MADE BY GENABOX 29/11/2023 --
+                 
+#                *        *        *        *       
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^#
